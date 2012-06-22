1852ab1ab93fc3658144ad3fc023d7edc022c8fc
Thomas Clausen <kontakt@thomasclausen.dk>
2012-06-22 12:08:51 +0300

    No scale

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta http-equiv="content-type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
-<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<meta name="viewport" content="width=device-width, maximum-scale=1"/>
 <meta name="description" content="" />
 <meta property="og:description" content="" />
 <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/images/logo-facebook.jpg">
