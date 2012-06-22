308a144235088c9af7a86e71a2af0641c2eab6c0
Thomas Clausen <kontakt@thomasclausen.dk>
2012-06-22 12:29:07 +0300

    Update dev-2.0

--- header.php
+++ header.php
@@ -9,6 +9,7 @@
 <title><?php wp_title( '', true, 'right' ); ?></title>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta http-equiv="content-type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
 <meta name="viewport" content="width=device-width, maximum-scale=1"/>
 <meta name="description" content="" />
 <meta property="og:description" content="" />
