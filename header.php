64985f1c008f9e4a1f372f99e1aa1f80b8a27af8
Thomas Clausen <kontakt@thomasclausen.dk>
2012-10-26 15:03:53 +0300

    Drop support for IE6 and move some header tags to functions.php

--- header.php
+++ header.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]><html class="no-js ie6" <?php language_attributes(); ?>><![endif]-->
 <!--[if IE 7]><html class="no-js ie7" <?php language_attributes(); ?>><![endif]-->
 <!--[if IE 8]><html class="no-js ie8" <?php language_attributes(); ?>><![endif]-->
 <!--[if IE 9]><html class="no-js ie9" <?php language_attributes(); ?>><![endif]-->
@@ -9,14 +8,7 @@
 <title><?php wp_title( '', true, 'right' ); ?></title>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta http-equiv="content-type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-<meta name="viewport" content="width=device-width, maximum-scale=1" />
-<meta name="description" content="" />
-<meta property="og:description" content="" />
-<meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/images/logo-facebook.jpg" />
-<link rel="image_src" href="<?php echo get_template_directory_uri(); ?>/images/logo-facebook.jpg" />
 <?php wp_head(); ?>
-<!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
 
 <body <?php body_class(); ?>>
