7c91ee957d78ed8563868d27c9233a6671b02fc0
Thomas Clausen <kontakt@thomasclausen.dk>
2013-08-02 14:11:28 +0200

    Use === instead of ==

--- content.php
+++ content.php
@@ -8,7 +8,7 @@
  */
 ?>
 
-		<?php if ( get_post_type() == 'page' ) : the_title( '<h1>', '</h1>' ); else : the_title( '<h2>' . __( 'FAKTA ', 'hovedspring-forbudt' ), '</h2>' ); endif; ?>
+		<?php if ( get_post_type() === 'page' ) : the_title( '<h1>', '</h1>' ); else : the_title( '<h2>' . __( 'FAKTA ', 'hovedspring-forbudt' ), '</h2>' ); endif; ?>
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> data-permalink="<?php the_permalink(); ?>">
 			<?php the_content(); ?>
 		</article>
